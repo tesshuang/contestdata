4496c34012df371fe6a1cdcb2f618c09c1df9a6f
Tess <tesshuangxj@yahoo.com>
2018-03-06 17:20:59 -0800

    change online url direction

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ con.connect(function(err) {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://contestdata.herokuapp.com/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
